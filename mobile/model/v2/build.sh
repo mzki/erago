810f38a5c244e14769a56c337c87d9a439edb1a3
mzki <28139969+mzki@users.noreply.github.com>
2022-11-20 22:42:03 +0900

    scripts: Add android to archive script

--- mobile/model/v2/build.sh
+++ mobile/model/v2/build.sh
@@ -2,12 +2,34 @@
 
 set -eu
 
-PRODUCT="erago-model-v2.aar"
+OUTPUTDIR=./
 
+while getopts d: opt; do
+	case "$opt" in
+		d)
+			OUTPUTDIR="$OPTARG"
+			;;
+		\?)
+			echo "Usage: build.sh [-d OUTPUTDIR]"
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+
+# build version
+VERSION=`git describe --tags --abbrev=0`
+VERSION_FOR_FILE=`echo "$VERSION" | sed -e "s/\./_/g"`
+COMMIT_HASH=`git rev-parse --short HEAD`
+# output path
+PRODUCT="erago_${VERSION_FOR_FILE}_android_model-v2.aar"
+OUTPUT=$OUTPUTDIR/$PRODUCT
+# flags
 BUILD_FLAGS="-gcflags=-trimpath=${GOPATH}"
 TARGET_FLAGS=android # can use "android/arm,android/amd64" to shrink data size
-
-echo "building ${PRODUCT}..."
+# build mobile library
+echo "building ${OUTPUT}..."
 # needs -androidapi 19 to use modern android SDK and NDK
 # See https://github.com/golang/go/issues/52470.
-gomobile bind ${BUILD_FLAGS} -androidapi 19 -target ${TARGET_FLAGS} -o ${PRODUCT} . || exit 1
+# TODO: Embed build version into binary like -x main.commit_hash=${COMMIT_HASH}?
+gomobile bind ${BUILD_FLAGS} -androidapi 19 -target ${TARGET_FLAGS} -o ${OUTPUT} . || exit 1
