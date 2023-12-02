df67fb6aff1dbe5307d21307f16c2c5137e6bfbd
mzki <28139969+mzki@users.noreply.github.com>
2023-12-02 12:09:31 +0000

    app: Fix scroll not working on multidisplay

--- app/ui.go
+++ app/ui.go
@@ -1,11 +1,14 @@
 package app
 
 import (
+	"image"
+
 	"golang.org/x/exp/shiny/unit"
 	"golang.org/x/exp/shiny/widget"
 	"golang.org/x/exp/shiny/widget/node"
 	"golang.org/x/exp/shiny/widget/theme"
 	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/mouse"
 
 	"github.com/mzki/erago/uiadapter"
 	customT "github.com/mzki/erago/view/exp/theme"
@@ -23,6 +26,9 @@ type UI struct {
 	// These Backgrounds fill each widgets by background color.
 	// To change color you can modify XXXBackground.ThemeColor.
 	CmdLineBackground *widget.Uniform
+
+	// for workaround
+	lastMouseEvent mouse.Event
 }
 
 // construct standard UI node tree for the era application.
@@ -65,4 +71,24 @@ func (ui *UI) OnLifecycleEvent(e lifecycle.Event) {
 // return erago/uiadapter.UI interface.
 func (ui UI) Editor() uiadapter.UI {
 	return ui.mv
+}
+
+func (ui *UI) OnInputEvent(e interface{}, origin image.Point) node.EventHandled {
+	// Workaround: In case of windows, multidisplay and mouse cursur on sub display which is left or above of primary display,
+	// point X and Y in mouse event with DirStep indicates 65522 or something.
+	// It seems uint16 underflow is occured (-1) --> (65535).
+	// But the event with other direction, X and Y are normal e.g. 0 <= x <= 1920 in 1080p screen.
+	// Here performs remembering last normal mouse event and replace abnormal X and Y by last normal one.
+	switch testEvent := e.(type) {
+	case mouse.Event:
+		if testEvent.Direction == mouse.DirNone {
+			ui.lastMouseEvent = testEvent
+		} else if testEvent.Direction == mouse.DirStep {
+			newMouseEvent := testEvent
+			newMouseEvent.X = ui.lastMouseEvent.X
+			newMouseEvent.Y = ui.lastMouseEvent.Y
+			e = newMouseEvent
+		}
+	}
+	return ui.ShellEmbed.OnInputEvent(e, origin)
 }
