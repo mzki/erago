24e06d15f61efedb44712ab4bf79edeafd7f01d8
mzki <28139969+mzki@users.noreply.github.com>
2024-10-19 15:42:02 +0000

    stub: Fix wrong timeout value and flaky testing

--- stub/ELA/game_test_input_request.lua
+++ stub/ELA/game_test_input_request.lua
@@ -1,6 +1,6 @@
 -- This line should return immidiately
 era.printl "game_test_input_request.lua"
-local ONE_SEC = 1* 1000 * 1000
+local ONE_SEC = 1* 1000 * 1000 * 1000
 local timeout = era.twait(ONE_SEC)
 if timeout then 
   error("user input is timeout!!!")
